d33df268a71a79d42f72747b05dbe82e8f849602
Julian Peters <julian.peters@gematik.de>
2021-04-30 13:23:16 +0200

    Proxy basic

--- tiger-proxy/src/main/java/de/gematik/test/tiger/proxy/TigerProxy.java
+++ tiger-proxy/src/main/java/de/gematik/test/tiger/proxy/TigerProxy.java
@@ -3,50 +3,53 @@ package de.gematik.test.tiger.proxy;
 import static org.mockserver.model.HttpOverrideForwardedRequest.forwardOverriddenRequest;
 import static org.mockserver.model.HttpRequest.request;
 
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import de.gematik.rbellogger.RbelLogger;
 import de.gematik.rbellogger.data.RbelElement;
 import de.gematik.test.tiger.proxy.configuration.TigerProxyConfiguration;
-import de.gematik.test.tiger.proxy.wiremockUtils.WiremockProxyUrlTransformer;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Optional;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
 import org.mockserver.client.MockServerClient;
-import org.mockserver.model.SocketAddress.Scheme;
 import org.mockserver.netty.MockServer;
 import org.mockserver.proxyconfiguration.ProxyConfiguration;
 import org.mockserver.proxyconfiguration.ProxyConfiguration.Type;
 
 @Slf4j
+@Data
 public class TigerProxy implements ITigerProxy {
 
     private final List<IRbelMessageListener> rbelMessageListeners = new ArrayList<>();
     private MockServer mockServer;
     private MockServerClient mockServerClient;
+    private RbelLogger rbelLogger;
 
     public TigerProxy(TigerProxyConfiguration configuration) {
-        ProxyConfiguration proxyConfiguration = ProxyConfiguration.proxyConfiguration(Type.HTTPS,
-            configuration.getForwardToProxy().getHostname()
-                + ":"
-                + configuration.getForwardToProxy().getPort());
-        mockServer = new MockServer(proxyConfiguration);
+        mockServer = convertProxyConfiguration(configuration)
+            .map(config -> new MockServer(config, 6666))
+            .orElse(new MockServer());
         mockServerClient = new MockServerClient("localhost", mockServer.getLocalPort());
-
-
-        //https://download-ref.tsl.ti-dienste.de/ECC/ECC-RSA_TSL-ref.xml'
-
-        mockServerClient.when(request()
-                .withHeader("Host", "tsl"))
-            .forward(
-                forwardOverriddenRequest(
-                    request()
-                        .withHeader("Host", "download-ref.tsl.ti-dienste.de")
-                ));
+        rbelLogger = RbelLogger.build();
 
         for (Entry<String, String> routeEntry : configuration.getProxyRoutes().entrySet()) {
             addRoute(routeEntry.getKey(), routeEntry.getValue());
         }
+    }
+
+    @NotNull
+    private Optional<ProxyConfiguration> convertProxyConfiguration(TigerProxyConfiguration configuration) {
+        if (configuration.getForwardToProxy() == null
+            || StringUtils.isEmpty(configuration.getForwardToProxy().getHostname())) {
+            return Optional.empty();
+        }
+        return Optional.of(ProxyConfiguration.proxyConfiguration(Type.HTTPS,
+            configuration.getForwardToProxy().getHostname()
+                + ":"
+                + configuration.getForwardToProxy().getPort()));
     }
 
     @Override
@@ -61,8 +64,7 @@ public class TigerProxy implements ITigerProxy {
 
     @Override
     public List<RbelElement> getRbelMessages() {
-//        return rbelLogger.getMessageHistory();
-        return null;
+        return rbelLogger.getMessageHistory();
     }
 
     @Override
@@ -73,6 +75,7 @@ public class TigerProxy implements ITigerProxy {
         mockServerClient.when(request()
             .withHeader("Host", sourceHost))
             .forward(
+                //  forward().withScheme("http")
                 forwardOverriddenRequest(
                     request()
                         .withHeader("Host", targetHost)
