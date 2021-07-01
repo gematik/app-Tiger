c8e3b8fabf08447804a9b3aacb37cdc7e43bac32
t.eitzenberger <thomas.eitzenberger@gematik.de>
2021-07-01 19:43:41 +0200

    adding local konsim config to testenv mgr

--- tiger-testenv-mgr/src/main/java/de/gematik/test/tiger/testenvmgr/config/CfgProductType.java
+++ tiger-testenv-mgr/src/main/java/de/gematik/test/tiger/testenvmgr/config/CfgProductType.java
@@ -5,5 +5,5 @@
 package de.gematik.test.tiger.testenvmgr.config;
 
 public enum CfgProductType {
-    IDP_REF, IDP_RISE, ERZP_FD_REF, EPA2_REF, EPA2_FDV_REF
+    IDP_REF, IDP_RISE, ERZP_FD_REF, EPA2_REF, EPA2_FDV_REF, KON_SIM
 }
