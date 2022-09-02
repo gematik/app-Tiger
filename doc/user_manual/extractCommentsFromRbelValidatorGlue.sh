696ec1da16e6500e028bc37ced2efb3a31d1c486
Dagmar Brünsing <dagmar.bruensing@gematik.de>
2022-09-02 09:46:58 +0000

    TGR-610: UserManual: Inhalte besser beschreiben

--- doc/user_manual/extractCommentsFromRbelValidatorGlue.sh
+++ doc/user_manual/extractCommentsFromRbelValidatorGlue.sh
@@ -2,7 +2,7 @@
 # ${GEMATIK_COPYRIGHT_STATEMENT}
 #
 
-sed 's/^ *//;s/ *$//' < ../../tiger-test-lib/src/test/java/de/gematik/test/tiger/glue/RBelValidatorGlue.java | \
+sed 's/^ *//;s/ *$//' < ../../tiger-test-lib/src/main/java/de/gematik/test/tiger/glue/RBelValidatorGlue.java | \
 grep -e "^[\*|\/|\@]" | \
 grep -ve "^//" | \
 sed 's/^\/\*/\n\//' | \
