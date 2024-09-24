e2aecf110d290428999fd093e0312607fe8f80b9
Dagmar Brünsing <dagmar.bruensing@gematik.de>
2024-09-24 13:39:39 +0200

    TGR-1573: fix failing uitest

--- tiger-uitests/src/test/java/de/gematik/test/tiger/playwright/workflowui/ZQuitTests.java
+++ tiger-uitests/src/test/java/de/gematik/test/tiger/playwright/workflowui/ZQuitTests.java
@@ -192,7 +192,7 @@ class ZQuitTests extends AbstractBase {
         () ->
             assertThat(
                     page.frameLocator("#rbellog-details-iframe").locator("#filteredMessage"))
-                .hasText("Filter didn't match any of the %d messages.".formatted(TOTAL_MESSAGES)));
+                .hasText("Filter matched to all of the %d messages.".formatted(TOTAL_MESSAGES)));
     page.frameLocator("#rbellog-details-iframe").locator("#filterModalButtonClose").click();
     assertThat(
             page.frameLocator("#rbellog-details-iframe").locator("#filterModalDialog")).not().isVisible();
