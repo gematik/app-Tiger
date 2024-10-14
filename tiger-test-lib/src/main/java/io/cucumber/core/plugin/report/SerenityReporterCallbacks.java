1075357bc25455abe6d893fb67b24248e808114a
Thomas Eitzenberger <thomas.eitzenberger@gematik.de>
2024-10-14 10:50:37 +0000

    TGR-1603: do not throw an exception if scenario state is erroneous, poms got license headers

--- tiger-test-lib/src/main/java/io/cucumber/core/plugin/report/SerenityReporterCallbacks.java
+++ tiger-test-lib/src/main/java/io/cucumber/core/plugin/report/SerenityReporterCallbacks.java
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package io.cucumber.core.plugin.report;
@@ -592,9 +593,9 @@ public class SerenityReporterCallbacks {
     switch (scenarioStatus) {
       case "PASSED" -> scPassed++;
       case "ERROR", "FAILED" -> scFailed++;
+      case "UNDEFINED" -> { /* ignore */ }
       default ->
-          throw new UnsupportedOperationException(
-              "Unsupported scenario: %s".formatted(scenarioStatus));
+          log.warn("Unsupported scenario state: %s".formatted(scenarioStatus));
     }
     log.info(
         "------------ STATUS: {} passed {}",
