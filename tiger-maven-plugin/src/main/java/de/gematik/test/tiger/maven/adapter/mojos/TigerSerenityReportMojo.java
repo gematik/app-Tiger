d950972bd224e7fda535c1972aacdf32f7122220
t.eitzenberger <thomas.eitzenberger@gematik.de>
2025-03-31 12:22:30 +0200

    TGR-1799: backup original json report files for use by Tiger testsuite validator in Titus

--- tiger-maven-plugin/src/main/java/de/gematik/test/tiger/maven/adapter/mojos/TigerSerenityReportMojo.java
+++ tiger-maven-plugin/src/main/java/de/gematik/test/tiger/maven/adapter/mojos/TigerSerenityReportMojo.java
@@ -12,10 +12,12 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package de.gematik.test.tiger.maven.adapter.mojos;
 
+import de.gematik.test.tiger.common.exceptions.TigerOsException;
 import de.gematik.test.tiger.common.web.TigerBrowserUtil;
 import de.gematik.test.tiger.maven.reporter.TigerHtmlReporter;
 import de.gematik.test.tiger.maven.reporter.TigerJsonReporter;
@@ -23,10 +25,12 @@ import de.gematik.test.tiger.maven.reporter.TigerReporter;
 import de.gematik.test.tiger.maven.reporter.TigerSinglePageReporter;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import net.thucydides.model.domain.ReportData;
@@ -36,6 +40,7 @@ import net.thucydides.model.reports.OutcomeFormat;
 import net.thucydides.model.reports.TestOutcomeLoader;
 import net.thucydides.model.reports.TestOutcomes;
 import net.thucydides.model.reports.json.JSONTestOutcomeReporter;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.text.StringEscapeUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -82,6 +87,8 @@ public class TigerSerenityReportMojo extends AbstractMojo {
       getLog().warn("Report directory does not exist yet: " + reportDirectory);
       return;
     }
+
+    backupJsonReportFiles();
     modifyStepDescriptionsWithTigerResolvedValues();
 
     createReporters()
@@ -109,6 +116,30 @@ public class TigerSerenityReportMojo extends AbstractMojo {
       reporters.add(new TigerJsonReporter(reportDirectory.toPath()));
     }
     return reporters;
+  }
+
+  private void backupJsonReportFiles() throws IOException {
+    Path unresolvedFolder = Path.of(reportDirectory.getAbsolutePath(), "unresolvedReports");
+    if (unresolvedFolder.toFile().exists()) {
+      FileUtils.deleteDirectory(unresolvedFolder.toFile());
+    }
+    Files.createDirectories(Path.of(reportDirectory.getAbsolutePath(), "unresolvedReports"));
+    try (Stream<Path> files = Files.list(reportDirectory.toPath())) {
+      files
+          .filter(path -> path.toString().endsWith(".json"))
+          .forEach(
+              path -> {
+                try {
+                  Files.copy(
+                      path,
+                      unresolvedFolder.resolve(path.getFileName()),
+                      java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                  throw new TigerOsException(
+                      "Unable to copy JSON report file " + path.toFile().getAbsolutePath(), e);
+                }
+              });
+    }
   }
 
   private void modifyStepDescriptionsWithTigerResolvedValues() throws IOException {
