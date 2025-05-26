81fa7765aa4aa34a5a874b5d6cde81bff9075166
Thomas Eitzenberger <thomas.eitzenberger@gematik.de>
2025-05-26 16:49:51 +0200

    TGR-1863: fix ui tests

--- tiger-uitests/src/test/java/de/gematik/test/tiger/playwright/workflowui/main/XDynamicSidebarTests.java
+++ tiger-uitests/src/test/java/de/gematik/test/tiger/playwright/workflowui/main/XDynamicSidebarTests.java
@@ -1,6 +1,5 @@
 /*
- *
- * Copyright 2021-2025 gematik GmbH
+ * Copyright 2024 gematik GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * *******
- *
- * For additional notes and disclaimer from gematik and in case of changes by gematik find details in the "Readme" file.
  */
 package de.gematik.test.tiger.playwright.workflowui.main;
 
@@ -182,6 +178,11 @@ class XDynamicSidebarTests extends AbstractBase {
 
   @Test
   void testExecutionPaneScenariosExists() {
+    // wait for up to 30 seconds for the other thread to finish execution of second feature file
+    await()
+        .atMost(120, TimeUnit.SECONDS)
+        .pollInterval(1, TimeUnit.SECONDS)
+        .until(() -> page.locator(".test-execution-pane-feature-title").count() == 2);
     assertAll(
         () -> assertThat(page.locator(".test-execution-pane-feature-title")).hasCount(2),
         () ->
--- tiger-uitests/src/test/java/de/gematik/test/tiger/playwright/workflowui/main/XYDynamicRbelLogTests.java
+++ tiger-uitests/src/test/java/de/gematik/test/tiger/playwright/workflowui/main/XYDynamicRbelLogTests.java
@@ -1,6 +1,5 @@
 /*
- *
- * Copyright 2021-2025 gematik GmbH
+ * Copyright 2024 gematik GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * *******
- *
- * For additional notes and disclaimer from gematik and in case of changes by gematik find details in the "Readme" file.
  */
 package de.gematik.test.tiger.playwright.workflowui.main;
 
@@ -317,6 +313,6 @@ class XYDynamicRbelLogTests extends AbstractBase {
     }
     Assertions.assertThat(ctr)
         .isLessThanOrEqualTo(200)
-        .withFailMessage("Pressed page down 20 time3s but didnt reach end of rbel log list!");
+        .withFailMessage("Pressed page down 20 times but didnt reach end of rbel log list!");
   }
 }
